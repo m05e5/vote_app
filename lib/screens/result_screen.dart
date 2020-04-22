93b4d54a1acaab76c832982e55fd4d15f5cd0257
m05e5 <calebmoses1111@gmail.com>
2020-04-23 01:03:20 +0200

    updating the result page

--- lib/screens/result_screen.dart
+++ lib/screens/result_screen.dart
@@ -1,10 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:charts_flutter/flutter.dart' as charts;
+import 'package:provider/provider.dart';
+import 'package:vote/state/vote.dart';
+import 'package:vote/models/vote.dart';
 
 class ResultScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container( 
-      
+      child: createChart(context),
     );
   }
+
+  Widget createChart(BuildContext context){
+    return charts.BarChart(
+      retrieveVoteResult(context),
+      animate: true,
+    );
+  }
+  List<charts.Series<VoteData, String>>  retrieveVoteResult(){};
+}
+
+class VoteData {
+  final String option;
+  final int total;
+
+  VoteData(this.option, this.total);
 }
